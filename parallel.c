0bf8d1ce172461fab82bcab7353c53ec148982c7
DonutCrab <dev@donutcrab.com>
2024-11-20 14:14:42 +0900

    try on parallel

--- parallel.c
+++ parallel.c
@@ -1,45 +1,33 @@
-// TODO: ...
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#include <png.h> // libpng
+#include <mpi.h>
 
 #include "qr.c"
 #include "image.c"
+#include "util.c"
 
-// cc sequential.c -o out/sequential -I/opt/homebrew/include -L/opt/homebrew/lib -lpng && ./out/sequential
+// mpicc parallel.c -o out/parallel -I/opt/homebrew/include -L/opt/homebrew/lib -lpng && mpirun -np 4 ./out/parallel
 
-void copy_row(float *from, float *to, int width)
-{
-    for (int i = 0; i < width; i++)
-    {
-        to[i] = from[i];
-    }
-}
-
-int main()
+int main(int argc, char *argv[])
 {
     float k = 0.3;
-    int steps = 1;
+    int steps = 20;
 
-    int size = 66;
-    float input_array[size][size];
+    int matrix_size = 35;
+    float input_matrix[matrix_size][matrix_size];
 
-    // float boundary_condition = 0.0;
-    // boundary_condition constant?
+    int pixel_multiplier = matrix_size / qr_size;
+    int offset = (matrix_size - (pixel_multiplier * qr_size)) / 2;
 
-    int pixel_multiplier = size / qr_size;
-    int offset = (size - (pixel_multiplier * qr_size)) / 2;
-
-    // fill input_array
-    for (int i = 0; i < size; i++)
+    // fill input_matrix
+    for (int i = 0; i < matrix_size; i++)
     {
-        for (int j = 0; j < size; j++)
+        for (int j = 0; j < matrix_size; j++)
         {
             if (i < offset || j < offset)
             {
-                input_array[i][j] = 0.0;
+                input_matrix[i][j] = 0.0;
             }
             else
             {
@@ -48,69 +36,87 @@ int main()
 
                 if (qr_i >= (qr_size) || qr_j >= (qr_size))
                 {
-                    input_array[i][j] = 0.0;
+                    input_matrix[i][j] = 0.0;
                 }
                 else
                 {
-                    input_array[i][j] = qr[qr_i][qr_j];
+                    input_matrix[i][j] = qr[qr_i][qr_j];
                 }
             }
         }
     }
 
-    // print input_array
-    // for (int i = 0; i < size; i++)
-    // {
-    //     for (int j = 0; j < size; j++)
-    //     {
-    //         printf("%s", input_array[i][j] > 0.5 ? " " : "\u25A0");
-    //     }
-    //     printf("\n");
-    // }
+    int mpi_rank, mpi_size;
 
-    float cache_row[size];
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
+
+    int rows_per_process = (matrix_size - 2) / mpi_size;
+
+    printf("to calc: %d\n", rows_per_process * mpi_size);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
     float cache_value;
+
+    int process_cache_size = rows_per_process + 2;
+    float process_cache[process_cache_size][matrix_size];
+
+    // start values for each process
+    for (int i = 0; i < process_cache_size; i++)
+    {
+        copy_row(input_matrix[i + mpi_rank], process_cache[i], matrix_size);
+    }
 
     for (int time = 0; time < steps; time++)
     {
-        copy_row(input_array[0], cache_row, size); // cache
+        int start = mpi_rank * mpi_size + 1;
+        int end = (mpi_rank + 1) * mpi_size;
 
-        // skip borders -> boundary condition
-        for (int row = 1; row < size; row++)
+        for (int row = start; row <= end; row++)
         {
-            cache_value = input_array[row][0]; // cache
+            printf("process: %d; step: %d; row: %d\n", mpi_rank, time, row);
 
-            // if (row > 0 && row < size)
-            // {
-            //     printf("calc_row %d\n", row);
-            // }
+            cache_value = input_matrix[row][0];
 
-            float new_row[size]; // new
-
-            for (int col = 0; col <= size; col++)
+            for (int col = 1; col < matrix_size - 1; col++)
             {
-                // skip borders
-                if (col > 0 && col < size - 1)
-                {
-                    float new_x = k * cache_row[col] + 2 * (1 - k) * input_array[row][col] + k * input_array[row + 1][col];
-                    float new_y = k * cache_value + 2 * (1 - k) * input_array[row][col] + k * input_array[row][col + 1];
-                    float new_value = (new_x + new_y) / 4;
+                float new_y = k * input_matrix[row - 1][col] + 2 * (1 - k) * input_matrix[row][col] + k * input_matrix[row + 1][col];
+                float new_x = k * input_matrix[row][col - 1] + 2 * (1 - k) * input_matrix[row][col] + k * input_matrix[row][col + 1];
+                float new_value = (new_x + new_y) / 4;
 
-                    cache_value = input_array[row][col]; // cache
-                    new_row[col] = new_value;            // overwrite
-                }
-                else
-                {
-                    // cache_value = input_array[row][col]; // cache
-                }
+                cache_value = input_matrix[row][col];
+                input_matrix[row][col] = new_value;
             }
+        }
 
-            copy_row(input_array[row], cache_row, size); // cache
-            copy_row(new_row, input_array[row], size);   // overwrite
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        // exchange top boundary
+        if (mpi_rank > 0)
+        {
+            MPI_Send(process_cache[0], matrix_size, MPI_FLOAT, mpi_rank - 1, 0, MPI_COMM_WORLD);
+            MPI_Recv(process_cache[0], matrix_size, MPI_FLOAT, mpi_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+
+        // exchange bottom boundary
+        if (mpi_rank < mpi_size - 1)
+        {
+            MPI_Send(process_cache[process_cache_size - 1], matrix_size, MPI_FLOAT, mpi_rank + 1, 0, MPI_COMM_WORLD);
+            MPI_Recv(process_cache[process_cache_size - 1], matrix_size, MPI_FLOAT, mpi_rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
     }
 
-    export_image("setpss", size, input_array);
+    // collect
+    for (int i = 1; i < process_cache_size - 1; i++)
+    {
+        copy_row(process_cache[i], input_matrix[i + mpi_rank], matrix_size);
+    }
+
+    MPI_Finalize();
+
+    export_image("parallel_end", matrix_size, input_matrix);
 
     return 0;
 }
